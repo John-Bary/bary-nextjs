5a1d4615e912e85f492fed15b9fca81f29ab6fe2
John-Bary <jonas.barysas@gmail.com>
2025-12-12 13:41:32 +0200

    Add Radix color palette and update theme

---
+++ vendor/radix-colors/index.d.ts
@@ -0,0 +1,6 @@
+export declare const blue: Record<string, string>;
+export declare const orange: Record<string, string>;
+export declare const amber: Record<string, string>;
+export declare const plum: Record<string, string>;
+export declare const teal: Record<string, string>;
+export declare const slate: Record<string, string>;
---
+++ vendor/radix-colors/index.js
@@ -0,0 +1,98 @@
+const blue = {
+  blue1: "#f5faff",
+  blue2: "#edf6ff",
+  blue3: "#e1f0ff",
+  blue4: "#cee7fe",
+  blue5: "#b7d9f8",
+  blue6: "#96c7f2",
+  blue7: "#5eb1ef",
+  blue8: "#0090ff",
+  blue9: "#0081f1",
+  blue10: "#006adc",
+  blue11: "#002f5d",
+  blue12: "#001634",
+};
+
+const orange = {
+  orange1: "#fef8f4",
+  orange2: "#fff1e7",
+  orange3: "#ffe8d7",
+  orange4: "#ffddc3",
+  orange5: "#ffc49f",
+  orange6: "#ffaa7f",
+  orange7: "#ff8b5f",
+  orange8: "#f76808",
+  orange9: "#ed5f00",
+  orange10: "#bd4b00",
+  orange11: "#451e11",
+  orange12: "#341911",
+};
+
+const amber = {
+  amber1: "#fefdfb",
+  amber2: "#fff9ed",
+  amber3: "#fff4ce",
+  amber4: "#ffe8a3",
+  amber5: "#ffdd87",
+  amber6: "#ffd176",
+  amber7: "#f3ba4d",
+  amber8: "#ee9d2b",
+  amber9: "#ffb224",
+  amber10: "#ffa01c",
+  amber11: "#ad5700",
+  amber12: "#4f3422",
+};
+
+const plum = {
+  plum1: "#fefcff",
+  plum2: "#fff7fb",
+  plum3: "#fbe8f7",
+  plum4: "#f7d5ef",
+  plum5: "#f0b9e6",
+  plum6: "#e093d8",
+  plum7: "#c978c9",
+  plum8: "#b650b1",
+  plum9: "#ab4aba",
+  plum10: "#a43cb4",
+  plum11: "#6f1d7a",
+  plum12: "#3b0f41",
+};
+
+const teal = {
+  teal1: "#f6fefd",
+  teal2: "#effefa",
+  teal3: "#e5fbf5",
+  teal4: "#d6f6ee",
+  teal5: "#c3eee4",
+  teal6: "#a5e4d4",
+  teal7: "#7dd4c0",
+  teal8: "#40c4aa",
+  teal9: "#12a594",
+  teal10: "#0b8b7f",
+  teal11: "#067a6f",
+  teal12: "#10302b",
+};
+
+const slate = {
+  slate1: "#fbfcfd",
+  slate2: "#f8f9fa",
+  slate3: "#f1f3f5",
+  slate4: "#eceef0",
+  slate5: "#e6e8eb",
+  slate6: "#dfe3e6",
+  slate7: "#d7dbdf",
+  slate8: "#c1c8cd",
+  slate9: "#889096",
+  slate10: "#7e868c",
+  slate11: "#687076",
+  slate12: "#11181c",
+};
+
+module.exports = {
+  blue,
+  orange,
+  amber,
+  plum,
+  teal,
+  slate,
+};
